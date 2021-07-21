f53e814edeb84729a94e23d8c45f9f84c4d46003
jordan-hess <jchno116012003@gmail.com>
2021-07-21 14:06:44 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def get_joke():
+
+    api_url = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(api_url).json()
+
+    return "<strong>Chuck Joke:</strong>" + response['value']
+
